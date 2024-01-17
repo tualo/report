8644c7ab5d06ada82d3ba3b12ead4310c1a3fba5
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-17 22:47:27 +0100

    set checksums

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -15,13 +15,13 @@ class StoredProcedures extends PostCheck {
         and routine_name in ('getReport','getReportPayments','getReportPositions','getReportReductions','getReportLocks','setReport','setReportDefaults','setReportHeader','setReportPosition','setReportTSE')
         */
         $def = [
-            'getReport'=>'3a0b505dc2aa1b26abfba88b4d33c03e',
+            'getReport'=>'724b39bc22f059d8b839ac8b8c89544f',
             'getReportPayments'=>'b23033b47b22b0239d557753583e4559',
             'getReportPositions'=>'d8467e62cd59ac0409aa080cd4fba0c5',
             'getReportReductions'=>'8e77e1a31b9301ff1e3d91dc518ee7e2',
             'getReportLocks'=>'7150f9793457fe532dad4cabe15b3529',
-            'setReport'=>'9b621e0b1287df6f94e8669e4e0117bd',
-            'setReportDefaults'=>'2b02fb7daa2bf0279da4b82d8e9ea911',
+            'setReport'=>'155f73c55f519a61891bffe4a40b203b',
+            'setReportDefaults'=>'d4d6ffa57887a689162122a73109bb65',
             'setReportHeader'=>'7127ac7c9c3acc137cbefead2ebbfe80',
             'setReportPosition'=>'a72146349c3271bc48192f80ae044c81',
             'setReportTSE'=>'bb1bec5fef1e0f451c44dad44250662a'
