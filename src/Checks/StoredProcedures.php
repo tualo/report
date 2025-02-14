acc71352db824b05568564c0602f158d402ce044
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-02-14 06:45:53 +0100

    set md5

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -7,35 +7,36 @@ use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\TualoApplication as App;
 
 
-class StoredProcedures extends PostCheck {
-    public static function test(array $config){
+class StoredProcedures extends PostCheck
+{
+    public static function test(array $config)
+    {
         // print_r($config);
         /*
         select routine_name,md5(routine_definition) md5 from information_schema.routines WHERE routine_schema = database() 
         and routine_name in ('getReport','getReportPayments','getReportPositions','getReportReductions','getReportLocks','setReport','setReportDefaults','setReportHeader','setReportPosition','setReportTSE')
         */
         $def = [
-            'getReport'=>'724b39bc22f059d8b839ac8b8c89544f',
-            'getReportPayments'=>'b23033b47b22b0239d557753583e4559',
-            'getReportPositions'=>'d8467e62cd59ac0409aa080cd4fba0c5',
-            'getReportReductions'=>'8e77e1a31b9301ff1e3d91dc518ee7e2',
-            'getReportLocks'=>'7150f9793457fe532dad4cabe15b3529',
-            'setReport'=>'b437ac7e4aab60e91eca5056b2d90fc5',
-            'setReportDefaults'=>'d4d6ffa57887a689162122a73109bb65',
-            'setReportHeader'=>'7127ac7c9c3acc137cbefead2ebbfe80',
-            'setReportPosition'=>'a72146349c3271bc48192f80ae044c81',
-            'setReportTSE'=>'bb1bec5fef1e0f451c44dad44250662a'
+            'getReport' => '1474b327465f7b11bd4e4de2876e2484',
+            'getReportPayments' => '3bd43a9909b25d2f7e2f6afa8dc5255a',
+            'getReportPositions' => '6253cb4d814889bfe6ad6b7107c498ac',
+            'getReportReductions' => '2c1d342d86da56c5c661732c9ed30136',
+            'getReportLocks' => '53f41a525bce4b69b77ed6c66669db82',
+            'setReport' => '1f7e7e5dcb228e57d57aa55a9489e973',
+            'setReportDefaults' => '4668d06bc97e7ab4134e09a996727de2',
+            'setReportHeader' => '406111d94654461257333bdd355a57c5',
+            'setReportPosition' => '6e6581f68c1a405dfbc6c922e48e8035',
+            'setReportTSE' => '93112627a04333a7b3f16867e7aa5bf5'
         ];
 
-                        //);
+        //);
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
         self::procedureCheck(
             'report',
             $def,
-            "please run the following command: `./tm install-sql-report --client ".$clientdb->dbname."`",
-            "please run the following command: `./tm install-sql-report --client ".$clientdb->dbname."`"
+            "please run the following command: `./tm install-sql-report --client " . $clientdb->dbname . "`",
+            "please run the following command: `./tm install-sql-report --client " . $clientdb->dbname . "`"
         );
-        
     }
 }
