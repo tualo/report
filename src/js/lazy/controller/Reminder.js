36271f9917be65d28f9fd9e0841df1cd69b98754
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-03-19 07:50:17 +0100

    using note

--- src/js/lazy/controller/Reminder.js
+++ src/js/lazy/controller/Reminder.js
@@ -68,7 +68,7 @@ Ext.define('Tualo.report.lazy.controller.Reminder', {
                 my_store.getRange().forEach((row) => {
                     positions.push(Ext.create('Tualo.DataSets.model.View_editor_blg_pos_' + tabellenzusatz, {
                         article: row.get('belegnummer'),
-                        reference: row.get('reference'),
+                        note: row.get('reference'),
                         account: '0000',
                         amount: 1,
                         singleprice: row.get('offen'),
